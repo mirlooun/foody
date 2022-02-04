3a5399b1c6a91f96bfc7745bda9afeca866b751f
mirlooun <mirlooun.iv0ff@gmail.com>
2022-02-04 13:27:41 +0200

    Add header tabs

---
+++ screens/Home.tsx
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+import HeaderTabs from '../components/HeaderTabs';
+import { SearchBar } from '../components/SearchBar';
+
+
+export default function Home() {
+    return (
+        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+            <View style={{ backgroundColor: "white", padding: 15 }}>
+                <HeaderTabs />
+                <SearchBar />
+            </View>
+        </SafeAreaView>
+    );
+}
