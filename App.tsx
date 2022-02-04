51c21b94f3147ca8673259ec48dd4296329c95b7
mirlooun <mirlooun.iv0ff@gmail.com>
2022-02-04 11:29:13 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { ReactHTMLElement, useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+export interface NameEntity {
+  id?: number,
+  name: string
+}
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.body}>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
+    margin: 20,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 30
   },
+  header: {
+    fontSize: 22,
+    fontWeight: "bold"
+  },
+  text: {
+    fontSize: 18,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  textInput: {
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10
+  },
+  button: {
+    marginBottom: 10
+  }
 });
+
+export default App;
